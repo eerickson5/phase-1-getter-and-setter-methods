d16d12faf872e6bafc1b50a1c71ce675c1ec4e8f
Elise Erickson <elisece717@gmail.com>
2023-12-11 08:23:36 -0500

    adds Circle class with getter and setter functions for diameter, area, and circumference

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your Circle class here
+
+class Circle{
+
+    #radius
+
+    constructor(radius){
+        this.#radius = radius;
+    }
+
+    get radius(){
+        return this.#radius;
+    }
+
+    get diameter(){
+        return(this.#radius * 2);
+    }
+
+    set diameter(diameter){
+        this.#radius = diameter / 2;
+    }
+
+    get circumference(){
+        return(Math.PI * this.#radius * 2)
+    }
+
+    set circumference(circumference){
+        this.#radius = circumference / 2 / Math.PI
+    }
+
+    get area(){
+        return(Math.PI * this.#radius ** 2)
+    }
+
+    set area(area) {
+        this.#radius = Math.sqrt(area / Math.PI)
+    }
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,5 @@
 require ( './helpers.js' );
+const path = require('path');
 
 let circle;
 
